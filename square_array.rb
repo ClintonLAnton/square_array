ccd667a350961f1db18db023af909069dcdfc166
clinton <clinton.l.anton@gmail.com>
2020-10-29 11:09:53 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
-def square_array(array)
+def square_array(numbers)
   # your code here
+  new_numbers = []
+  numbers.each do |num| 
+    new_numbers <<  (num ** 2)
+    
+  end
+  return new_numbers
+
 end
+
+
+
